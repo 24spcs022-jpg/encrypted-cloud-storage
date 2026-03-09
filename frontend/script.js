4f9fd3498372758c5f03343124c6e9833fa9afa0
24spcs022-jpg <24spcs022@mannarcollege.ac.in>
2026-03-09 12:49:21 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,43 @@
+alert("JS Working");
+const API="http://127.0.0.1:5000";
+
+function register(){
+fetch(API+"/register",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username:ruser.value,password:rpass.value})
+})
+.then(r=>r.json()).then(d=>alert(d.msg));
+}
+
+function login(){
+fetch(API+"/login",{
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username:luser.value,password:lpass.value})
+})
+.then(r=>r.json())
+.then(d=>{
+localStorage.token=d.token;
+window.location="dashboard.html";
+});
+}
+
+function upload(){
+let fd=new FormData();
+fd.append("file",file.files[0]);
+fd.append("password",upass.value);
+
+fetch(API+"/upload",{
+method:"POST",
+headers:{token:localStorage.token},
+body:fd
+})
+.then(r=>r.json()).then(d=>alert(d.msg));
+}
+
+function loadDash(){
+fetch(API+"/dashboard",{headers:{token:localStorage.token}})
+.then(r=>r.json())
+.then(d=>out.innerHTML="Files: "+d.total_files);
+}
