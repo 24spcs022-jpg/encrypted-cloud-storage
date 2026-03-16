2bd3da77f15a00a3dfbbf4b994b058ab63303f1f
dinesh <24spcs022@mannarcollege.ac.in>
2026-03-16 12:14:06 +0530

    added chat share strong password

--- app.py
+++ app.py
@@ -345,4 +345,8 @@ def inbox():
 # ---------------- RUN ----------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
