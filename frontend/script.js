71ca6ff0c325496a5ba6fa1430470727bb870c84
dinesh <24spcs022@mannarcollege.ac.in>
2026-03-25 09:44:03 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -56,8 +56,13 @@ document.getElementById("otpBox").style.display="block"
 
 function verifyOTP(){
 
-let entered = otp.value
-let user = luser.value
+let entered = document.getElementById("otp").value
+let user = document.getElementById("luser").value
+
+if(!entered){
+alert("Enter OTP")
+return
+}
 
 if(entered !== generatedOTP){
 alert("Wrong OTP ❌")
@@ -71,15 +76,18 @@ fetch("/login",{method:"POST",body:f})
 .then(r=>r.json())
 .then(d=>{
 
+console.log("OTP LOGIN:", d)
+
 if(d.token){
 localStorage.setItem("user",user)
 localStorage.setItem("token",d.token)
 window.location="/dashboard"
 }else{
-alert("Login failed")
+alert("Login failed ❌")
 }
 
 })
+
 }
 
 function upload(){
